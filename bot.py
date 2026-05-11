7599db33ab52b01d8f4f2dd599b2ff4948cf2a1b
Caiomoreira2004 <166776022+Caiomoreira2004@users.noreply.github.com>
2026-05-11 20:42:23 -0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,15 +1,16 @@
 import discord
 import requests
 import re
-import os
 
-TOKEN = os.environ.get("MTUwMzUzNzI3NDQyNjQ5MDkwMA.GQiP3d.cvZgpPeGBrTFWS7-K1Mr_nXfYpmKLcdLW9swYw")
-PUSHCUT_API_KEY = os.environ.get("iJW7T1wzJjXB3hIwDjG7Z")
+TOKEN = "MTUwMzUzNzI3NDQyNjQ5MDkwMA.GQiP3d.cvZgpPeGBrTFWS7-K1Mr_nXfYpmKLcdLW9swYw"
+
+PUSHCUT_API_KEY = "iJW7T1wzJjXB3hIwDjG7Z"
 
 CANAL_VENDAS = 1497420405894283479
 CANAL_PERGUNTAS = 1497420606574952559
 
 PUSHCUT_VENDAS = "https://api.pushcut.io/iJW7T1wzJjXB3hIwDjG7Z/notifications/Venda%20%F0%9F%92%B0"
+
 PUSHCUT_PERGUNTAS = ""
 
 intents = discord.Intents.default()
@@ -23,14 +24,18 @@ async def on_ready():
 
 @client.event
 async def on_message(message):
+
     if message.author.bot:
         return
 
+    # CANAL DE VENDAS
     if message.channel.id == CANAL_VENDAS:
+
         texto = message.content or ""
 
         if not texto and message.embeds:
             for embed in message.embeds:
+
                 texto += (embed.title or "") + "\n"
                 texto += (embed.description or "") + "\n"
 
@@ -40,6 +45,7 @@ async def on_message(message):
         match = re.search(r"R\$\s*[\d.,]+", texto)
 
         if match:
+
             valor = match.group(0)
 
             requests.post(
@@ -55,30 +61,45 @@ async def on_message(message):
 
             print(f"Venda enviada pro Pushcut: {valor}")
 
+    # CANAL DE PERGUNTAS
     elif message.channel.id == CANAL_PERGUNTAS:
+
         nome = "Cliente desconhecido"
+
         texto = message.content or ""
 
         if message.embeds:
+
             for embed in message.embeds:
+
                 for field in embed.fields:
+
                     if "cliente" in field.name.lower():
                         nome = field.value.strip()
                         break
 
                 if nome == "Cliente desconhecido" and embed.description:
-                    match_nome = re.search(r"Cliente\s*\n(.+)", embed.description)
+
+                    match_nome = re.search(
+                        r"Cliente\s*\n(.+)",
+                        embed.description
+                    )
 
                     if match_nome:
                         nome = match_nome.group(1).strip()
 
         if nome == "Cliente desconhecido" and texto:
-            match_nome = re.search(r"Cliente\s*\n(.+)", texto)
+
+            match_nome = re.search(
+                r"Cliente\s*\n(.+)",
+                texto
+            )
 
             if match_nome:
                 nome = match_nome.group(1).strip()
 
         if PUSHCUT_PERGUNTAS:
+
             requests.post(
                 PUSHCUT_PERGUNTAS,
                 json={
@@ -91,6 +112,7 @@ async def on_message(message):
             )
 
             print(f"Pergunta de {nome} enviada pro Pushcut")
+
         else:
             print("URL do Pushcut de perguntas não configurada.")
 
